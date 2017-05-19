4293974cfeedf477eab06636f3f94e1d5e747ac9
Kishor Tiwari <kishorsanu1994@gmail.com>
2017-05-20 09:09:43 +1200

    Added sample in-memory data.

---
+++ Omac.Data/SampleData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Omack.Data
+{
+    public class SampleData
+    {
+        public SampleData()
+        {
+            //initialize some in memory data inside constructor
+            Items = new List<Items>()
+            {
+                new Items{Id = 1, Name = "Curry", DatePurchased= DateTime.UtcNow, Price = 55},
+                new Items{Id = 2, Name = "Meat", DatePurchased= DateTime.UtcNow, Price = 15},
+                new Items{Id = 3, Name = "Milk", DatePurchased= DateTime.UtcNow, Price = 5},
+                new Items{Id = 4, Name = "Rent", DatePurchased= DateTime.UtcNow, Price = 450},
+                new Items{Id = 5, Name = "Veg", DatePurchased= DateTime.UtcNow, Price = 23},
+                new Items{Id = 6, Name = "Eggs", DatePurchased= DateTime.UtcNow, Price = 8},
+                new Items{Id = 7, Name = "Breads", DatePurchased= DateTime.UtcNow, Price = 1},
+            };
+        }
+        public static SampleData Current { get; set; } = new SampleData();
+        public List<Items> Items { get; set; }
+        
+    }
+  
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Omack.Api/Controllers/ValuesController.cs
+++ Omack.Api/Controllers/ValuesController.cs
@@ -3,42 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Omack.Data;
 
 namespace Omack.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]   //routing are of two type: 1. Conventional MVC Routing & Attribute Routing. This is attribute routing.
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [HttpGet()]
+        public JsonResult GetItems()
         {
-            return new string[] { "value1", "value2" };
+            return new JsonResult(SampleData.Current.Items);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        //api/values/int
+        [HttpGet("{id}")]  //to work with parameters in routing, curley braces are used.
+        public JsonResult GetItem(int Id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new JsonResult(SampleData.Current.Items.FirstOrDefault(i => i.Id == Id));
         }
     }
 }
